5d3745766a9ef36f1e603ee3e3a456ef7669af52
Emiliano <emilianochocobar97@gmail.com>
2024-01-21 01:00:21 -0300

    wireframe

--- js/login.js
+++ js/login.js
@@ -1,6 +1,8 @@
 let usuario = 'administrador1@cineatlas.com';
 let contraseña = 'gtr234T';
 
+// En pagina Login se deben colocar este usuario y contraseña para poder ingresar a pagina Administrador. En la misma se podra encontrar una tabla que contiene la cantidad de personas que se van registrando para ser socios del cine y una api basica de peliculas
+
 const login = () =>{
 
     let email = document.getElementById('email').value;
